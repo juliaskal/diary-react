a8e43cddeabf3a13b94c6e04e4a19dba576f622f
Юлия Скалецкая <yuskaleckaya@rumicon.ru>
2025-12-22 17:05:25 +0300

    💄(app): Update layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,10 +1,7 @@
 import "@/styles/globals.css";
 import { Metadata, Viewport } from "next";
-import { Link } from "@heroui/link";
 import clsx from "clsx";
-
 import { Providers } from "./providers";
-
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Navbar } from "@/components/navbar";
@@ -14,7 +11,6 @@ export const metadata: Metadata = {
     default: siteConfig.name,
     template: `%s - ${siteConfig.name}`,
   },
-  description: siteConfig.description,
   icons: {
     icon: "/favicon.ico",
   },
@@ -42,22 +38,15 @@ export default function RootLayout({
         )}
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+
           <div className="relative flex flex-col h-screen">
+
             <Navbar />
+
             <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
               {children}
             </main>
-            <footer className="w-full flex items-center justify-center py-3">
-              <Link
-                isExternal
-                className="flex items-center gap-1 text-current"
-                href="https://heroui.com?utm_source=next-app-template"
-                title="heroui.com homepage"
-              >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">HeroUI</p>
-              </Link>
-            </footer>
+
           </div>
         </Providers>
       </body>
