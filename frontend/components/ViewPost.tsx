8d4d742a969b55ba12af5712d19eea8f600ec02e
Юлия Скалецкая <yuskaleckaya@rumicon.ru>
2025-12-23 15:22:27 +0300

    💄(components): Update ViewPost component

--- frontend/components/ViewPost.tsx
+++ frontend/components/ViewPost.tsx
@@ -1,9 +1,14 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { Card, CardHeader, CardBody } from "@heroui/card";
+import { Button } from "@heroui/button";
+import Link from "next/link";
 import { title } from "@/components/primitives";
-import { PostCard } from "@/components/PostCard";
+import { Trash2, Pen } from "lucide-react";
 import type { Post } from "@/types/post";
+import { QuillDeltaToHtmlConverter } from "quill-delta-to-html";
+import 'react-quill/dist/quill.snow.css';
 import { Spinner } from "@heroui/spinner";
 
 type ViewPostProps = { postId: string };
@@ -32,10 +37,62 @@ export function ViewPost({ postId }: ViewPostProps) {
 
   if (!post) return <div>Запись не найдена</div>;
 
+  const formattedDate = new Date(post.created_at).toLocaleString("ru-RU", {
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  const delta = JSON.parse(post.content);
+
+  const converter = new QuillDeltaToHtmlConverter(delta, {
+    inlineStyles: true,
+  });
+
+  const html = converter.convert();
+
   return (
-    <div>
-      <h1 className={title()}>запись</h1>
-      <PostCard key={post.id} post={post} />
+    <div className="flex flex-col gap-10">
+
+    <h1 className={title()}>{post.title}</h1>
+
+    <Card className="mb-4 p-3">
+      {/* HEADER */}
+      <CardHeader className="flex justify-between items-start gap-4">
+        <div>
+          <p className="text-sm text-default-500">{formattedDate}</p>
+        </div>
+
+        <div className="flex gap-1">
+          <Button isIconOnly size="sm" variant="light" color="danger">
+            <Trash2 className="w-4 h-4" />
+          </Button>
+          <Button isIconOnly size="sm" variant="light">
+            <Pen className="w-4 h-4" />
+          </Button>
+        </div>
+      </CardHeader>
+
+      {/* BODY */}
+      <CardBody>
+        <div className={`text-default-700 whitespace-pre-line`}
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      </CardBody>
+    </Card>
+
+    <Link href="/">
+      <Button
+          size="sm"
+          className="bg-linear-to-tr from-rose-300 to-pink-400 shadow-lg tracking-widest px-8"
+          radius="full"
+      >
+        на главную
+      </Button>
+    </Link>
+
     </div>
   );
 }
