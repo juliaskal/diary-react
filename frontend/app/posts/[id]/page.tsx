65427a05499e3bdbe8deb4f9f08f2f4c226cf54f
Юлия Скалецкая <yuskaleckaya@rumicon.ru>
2025-12-23 14:26:39 +0300

    ✨(app): Add page to app posts [id]

---
+++ frontend/app/posts/[id]/page.tsx
@@ -0,0 +1,11 @@
+import { ViewPost } from "@/components/ViewPost";
+
+type Props = {
+  params: Promise<{ id: string }>;
+};
+
+export default async function PostPage({ params }: Props) {
+  const { id } = await params;
+
+  return <ViewPost postId={id} />;
+}
