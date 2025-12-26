5b9733aa36b3ccbbf5cdd7c49f564694a9022792
Юлия Скалецкая <yuskaleckaya@rumicon.ru>
2025-12-26 17:25:16 +0300

    🏷️(types): Update type of content field of Post interface

--- frontend/types/post.ts
+++ frontend/types/post.ts
@@ -1,7 +1,9 @@
+import type { DeltaStatic } from 'quill';
+
 export interface Post {
   id: number;
   title: string | null;
-  content: string;
+  content: DeltaStatic;
   created_at: string;
   folder: string | null;
 }
