a914c44bef74db414e7ab84eaea93cc745e805c3
Юлия Скалецкая <yuskaleckaya@rumicon.ru>
2025-12-26 17:29:56 +0300

    ✨(app): Create page to creating post

---
+++ frontend/app/posts/new/page.tsx
@@ -0,0 +1,12 @@
+import { title } from "@/components/primitives";
+import clsx from "clsx";
+import { PostForm } from "@/components/PostForm";
+
+export default function AddPost() {
+  return (
+    <div className="flex flex-col gap-10">
+      <h1 className={clsx(title(), "tracking-wider")}>создать запись</h1>
+      <PostForm post={null} />
+    </div>
+  );
+}
