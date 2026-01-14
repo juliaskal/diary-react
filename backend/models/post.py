6246967adc5dbca4f16d8e3b7a1d2487528a1a56
Юлия Скалецкая <yuskaleckaya@rumicon.ru>
2026-01-14 11:59:38 +0300

    🏷️(models): Change type of folder field

--- backend/models/post.py
+++ backend/models/post.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 from models import FreezeModel
+from models.folder import Folder
 
 
 class Post(FreezeModel):
     id: str | None = None
     title: str | None = None
     created_at: datetime
-    folder: str | None = None
+    folder: Folder
     content: dict
     is_archived: bool = False
     is_deleted: bool = False
