bfb601a906b95b5326ca32bb9f1dd65d3674dfc0
Юлия Скалецкая <yuskaleckaya@rumicon.ru>
2026-01-14 12:06:24 +0300

    🚧(components): Add onDelete to props

--- frontend/components/PostList.tsx
+++ frontend/components/PostList.tsx
@@ -5,13 +5,14 @@ import { Pagination } from "@heroui/pagination";
 
 interface PostListProps {
   posts: Post[];
+  onDelete: (postId: string) => void;
 }
 
-export function PostList({ posts }: PostListProps) {
+export function PostList({ posts, onDelete }: PostListProps) {
   return (
     <div>
       {posts.map((post) => (
-        <PostCard key={post.id} post={post} />
+        <PostCard key={post.id} post={post} onDelete={onDelete}/>
       ))}
 
     <footer className="w-full flex items-center justify-center py-3">
