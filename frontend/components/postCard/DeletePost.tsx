b641ed5898f3aeb174f6ae4ba86015376a48fe5e
Юлия Скалецкая <yuskaleckaya@rumicon.ru>
2026-01-14 12:07:42 +0300

    ✨ (components): Create DeletePost component

---
+++ frontend/components/postCard/DeletePost.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { Button } from "@heroui/button";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+} from "@heroui/modal";
+import { Trash2 } from "lucide-react";
+import { siteConfig } from "@/config/site";
+
+interface DeletePostProps {
+  postId: string;
+  onDeleted: () => void;
+}
+
+export function DeletePost({ postId, onDeleted  }: DeletePostProps) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  async function handleDelete() {
+    try {
+      await fetch(`${siteConfig.backendDomain}/api/posts/${postId}`, {
+        method: "DELETE",
+      });
+
+      onDeleted();
+      onClose();
+    } catch (error) {
+      console.error("Ошибка удаления поста", error);
+    }
+  }
+
+  return (
+    <>
+      {/* КНОПКА УДАЛЕНИЯ */}
+      <Button
+        isIconOnly
+        size="sm"
+        variant="light"
+        color="danger"
+        onPress={onOpen}
+      >
+        <Trash2 className="w-4 h-4" />
+      </Button>
+
+      {/* МОДАЛЬНОЕ ОКНО */}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalContent>
+          <ModalHeader>Удалить запись?</ModalHeader>
+
+          <ModalBody>
+            <p>
+              Вы уверены, что хотите удалить эту запись?
+              Это действие нельзя отменить.
+            </p>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+                variant="light"
+                className="shadow-lg tracking-widest px-8"
+                onPress={onClose}
+                radius="full"
+            >
+              отмена
+            </Button>
+
+            <Button
+                className="shadow-lg tracking-widest px-8"
+                onPress={handleDelete}
+                radius="full"
+                color="danger"
+            >
+              удалить
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
