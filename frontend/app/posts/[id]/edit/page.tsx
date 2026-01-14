f2f6a490c341ed283753d7b2a237216b23fc05f9
Юлия Скалецкая <yuskaleckaya@rumicon.ru>
2026-01-14 12:04:37 +0300

    ✨(frontend): Add page to edit post

---
+++ frontend/app/posts/[id]/edit/page.tsx
@@ -0,0 +1,36 @@
+import type { Post } from "@/types/post";
+import { PostForm } from "@/components/postForm/PostForm";
+import { siteConfig } from "@/config/site";
+import clsx from "clsx";
+import { title } from "@/components/primitives";
+
+type Props = {
+  params: { id: string };
+};
+
+async function getPost(id: string): Promise<Post> {
+  const res = await fetch(
+    `${siteConfig.backendDomain}/api/post/${id}`,
+    { cache: "no-store" }
+  );
+
+  if (!res.ok) {
+    throw new Error("Не удалось загрузить пост");
+  }
+
+  return res.json();
+}
+
+export default async function EditPostPage({ params }: Props) {
+  const post = await getPost(params.id);
+
+  return (
+    <div className="flex flex-col gap-10">
+      <h1 className={clsx(title(), "tracking-wider")}>
+        изменить запись
+      </h1>
+
+      <PostForm post={post} />
+    </div>
+  );
+}
