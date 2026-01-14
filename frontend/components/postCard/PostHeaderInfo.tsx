920d05f18683fe48d79e40d0776b6c3a8db3c4e7
Юлия Скалецкая <yuskaleckaya@rumicon.ru>
2026-01-14 12:07:52 +0300

    ✨ (components): Create PostHeaderInfo component

---
+++ frontend/components/postCard/PostHeaderInfo.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import type { Post } from "@/types/post";
+
+interface PostHeaderInfoProps {
+  post: Post;
+}
+
+export function PostHeaderInfo({ post }: PostHeaderInfoProps) {
+return (
+    <div className="flex">
+
+        <div className="flex-none border-1 border-default-200/50 rounded-small text-center w-11 overflow-hidden">
+
+        <div className="text-tiny bg-default-100 py-0.5 text-default-500">
+            {new Date(post.created_at).toLocaleString("ru-RU", {month: "short"})}
+        </div>
+
+        <div className="flex items-center justify-center font-semibold text-medium h-6 text-default-500">
+            {new Date(post.created_at).toLocaleString("ru-RU", {day: "2-digit"})}
+        </div>
+
+        </div>
+
+        <div className="ms-4">
+            <p className="text-sm text-default-500 font-semibold text-left">
+                {new Date(post.created_at).toLocaleString("ru-RU", {year: "numeric"})},
+                в {new Date(post.created_at).toLocaleString("ru-RU", {hour: "2-digit", minute: "2-digit"})}
+            </p>
+
+            <Link href="#">
+                <p className="text-left">
+                <span className="text-sm text-default-500">папка: </span>
+                <span className="text-medium font-medium text-blue-500">{post.folder.name}</span>
+                </p>
+            </Link>
+        </div>
+
+    </div>
+)}
