65dc35d47f8bd6728978fd47d011c2e7a8d050b2
Юлия Скалецкая <yuskaleckaya@rumicon.ru>
2026-01-30 18:09:59 +0300

    🚧 Change quill to sun-editor

--- frontend/components/Post/PostForm/PostForm.tsx
+++ frontend/components/Post/PostForm/PostForm.tsx
@@ -6,12 +6,12 @@ import 'react-quill/dist/quill.snow.css';
 import { Input } from "@heroui/input";
 import { DatePicker } from "@heroui/date-picker";
 import { now, getLocalTimeZone, ZonedDateTime } from "@internationalized/date";
-import Formatter from "@/components/Post/PostForm/Formatter";
 import Link from "next/link";
 import { siteConfig } from "@/config/site";
 import type { DeltaStatic } from 'quill';
 import { useRouter } from "next/navigation";
 import { FolderSelect } from "@/components/Post/PostForm/FolderSelect";
+import { Editor } from "@/components/Post/PostForm/SunEditor/Editor";
 
 interface PostFormProps {
   post: Post | null;
@@ -25,17 +25,11 @@ function PostForm({ post }: PostFormProps) {
   const [folder, setFolder] = useState<string | null>(post?.folder?.id ?? null);
   const [content, setContent] = useState<DeltaStatic | null>(post?.content ?? null);
 
-  const formatterRef = useRef<{
-    getContent: () => DeltaStatic | null;
-  }>(null);
-
   const handleSubmit = async () => {
-    const currentContent = formatterRef.current?.getContent() ?? content;
-
     const payload = {
       title,
       folder,
-      content: currentContent,
+      content,
       created_at: date?.toDate().toISOString(),
     };
 
@@ -85,7 +79,7 @@ function PostForm({ post }: PostFormProps) {
 
       <FolderSelect value={folder} onChange={setFolder}/>
 
-      <Formatter ref={formatterRef} onChange={setContent} />
+      <Editor content={"<div>" + JSON.stringify(post?.content) + "</div>"} />
 
       <div className="flex flex-wrap gap-4 items-center mt-4">
 
@@ -93,7 +87,7 @@ function PostForm({ post }: PostFormProps) {
           size="sm"
           className="bg-linear-to-tr from-rose-300 to-pink-400 shadow-lg tracking-widest px-8"
           radius="full"
-          onPress={handleSubmit}
+          // onPress={handleSubmit}
         >
           сохранить
         </Button>
---
+++ frontend/components/Post/PostForm/SunEditor/Editor.tsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import SunEditor from "suneditor-react";
+import "suneditor/dist/css/suneditor.min.css"; // Import Sun Editor's CSS File
+
+const defaultFonts = [
+  "Arial",
+  "Comic Sans MS",
+  "Courier New",
+  "Impact",
+  "Georgia",
+  "Tahoma",
+  "Trebuchet MS",
+  "Verdana"
+];
+
+interface EditorProps {
+  content: string | null;
+}
+
+export function Editor({ content }: EditorProps) {
+  const [value, setValue] = useState(content ?? "");
+
+  const sortedFontOptions = [
+    "Logical",
+    "Salesforce Sans",
+    "Garamond",
+    "Sans-Serif",
+    "Serif",
+    "Times New Roman",
+    "Helvetica",
+    ...defaultFonts
+  ].sort();
+
+  return (
+    <div className="App">
+      <SunEditor
+        setContents={value}
+        onChange={setValue}
+        setOptions={{
+          buttonList: [
+            ["undo", "redo"],
+            ["font", "fontSize"],
+            ['paragraphStyle', 'blockquote'],
+            [
+              "bold",
+              "underline",
+              "italic",
+              "strike",
+              "subscript",
+              "superscript"
+            ],
+            ["fontColor", "hiliteColor"],
+            ["align", "list", "lineHeight"],
+            ["outdent", "indent"],
+
+            ["table", "horizontalRule", "link", "image", "video"],
+            // ['math'] //You must add the 'katex' library at options to use the 'math' plugin.
+            // ['imageGallery'], // You must add the "imageGalleryUrl".
+            ["fullScreen", "showBlocks", "codeView"],
+            ["preview", "print"],
+            ["removeFormat"]
+
+            // ['save', 'template'],
+            // '/', Line break
+          ], // Or Array of button list, eg. [['font', 'align'], ['image']]
+          defaultTag: "div",
+          minHeight: "300px",
+          showPathLabel: false,
+          font: sortedFontOptions
+        }}
+      />
+    </div>
+  );
+}
