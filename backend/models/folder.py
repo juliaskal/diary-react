6f7abf8e72d2c6fc9c10246d9420faa534afb0d5
Юлия Скалецкая <yuskaleckaya@rumicon.ru>
2026-02-06 13:47:36 +0300

    🚧 Update fields in Folder model

--- backend/models/folder.py
+++ backend/models/folder.py
@@ -1,7 +1,10 @@
+from datetime import datetime
 from models import FreezeModel
 
 
 class Folder(FreezeModel):
     id: str | None = None
     name: str
-    icon: str = ""
+    icon: str | None = None
+    cover: str | None = None
+    created_at: datetime = datetime.now()
